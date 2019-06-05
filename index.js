d59f6d5b63d6812a8a77380072568633a138ee49
deepk <deep.kh.aish@gmail.com>
2019-06-05 14:53:36 -0400

    assignment

--- index.js
+++ index.js
@@ -11,13 +11,34 @@ db.serialize(function(){
     db.run("INSERT INTO Classroom VALUES('Watson',120,50)");
 
     db.run("CREATE Table Department(Department_Name TEXT,Building TEXT,Budget NUMBER)");
-    db.run("INSERT INTO Department VALUES('Biology','watson',90000)");
+    db.run("INSERT INTO Department VALUES('Biology','Watson',90000)");
     db.run("INSERT INTO Department VALUES('Comp Sci.','Taylor',90000)");
     db.run("INSERT INTO Department VALUES('Elec. Eng.','Taylor',90000)");
     db.run("INSERT INTO Department VALUES('Finance','Painter',90000)");
     db.run("INSERT INTO Department VALUES('History','Painter',90000)");
     db.run("INSERT INTO Department VALUES('Music','Packard',90000)");
     db.run("INSERT INTO Department VALUES('Physics','Painter',90000)");
+
+    db.each("SELECT Room_Number, Building FROM Classroom WHERE Capacity > 50",function(err,row){
+        if(err)
+            console.log(err);
+        console.log(row);
+    });
+
+
+    db.all("SELECT Department_Name FROM Department WHERE Budget > 85000",function(err,row){
+        if(err)
+            console.log(err);
+        console.log(row);
+    });
+    
+    db.each("SELECT Department.Department_Name, SUM(Capacity) AS Sum_Capacity FROM Classroom NATURAL JOIN Department GROUP BY Department.Department_Name",function(err,row){
+        if(err)
+            console.log(err);
+        console.log(row);
+    });
+    
+
 
 
 
